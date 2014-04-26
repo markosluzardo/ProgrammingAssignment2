9d8f4b89616f02df3cfa8c1d2672f194d81dca32
Marcos Luzardo <markos.luzardo@gmail.com>
2014-04-26 03:32:45 +0300

    Changed to manage caching of the inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Sets the constructor of the matrix cache object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## Default to NULL on creation
+    inv <- NULL
+    ## Assign new given matrix as global stored matrix and reset inverse
+    set <- function(y){
+        x <<- y
+        inv <<- NULL
+    }
+    ## Return matrix value
+    get <- function() x
+    ## Assign given matrix as global matrix inverse
+    setinverse <- function(solve) inv <<- solve
+    ## Return stored inverse matrix
+    getinverse <- function() inv
+    list(set = set, get = get, 
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Gets the stored matrix inverse and if not found
+## computes the inverse of matrix in makeCacheMatrix object 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Get the inverse of 'x'
+    inv <- x$getinverse()
+    ## If the stored inverse is not null, return the stored inverse
+    if (!is.null(inv)){
+        message("getting cached data")
+        return(inv)
+    }
+    ## In case the inverse is null, compute new inverse
+    data <- x$get()
+    inv <- solve(data,...)
+    ## Store the new inverse in 'x'
+    x$setinverse(inv)
+    inv
 }
